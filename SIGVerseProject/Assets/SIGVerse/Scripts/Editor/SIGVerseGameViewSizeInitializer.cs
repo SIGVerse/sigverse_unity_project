5be6d72653a74e59978a60ffb52b37d32282f714
hiroki-yamada <yamada4tec@gmail.com>
2020-03-23 18:14:30 +0900

    Add: Scripts of GameView Size Initializer

---
+++ SIGVerseProject/Assets/SIGVerse/Scripts/Editor/SIGVerseGameViewSizeInitializer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Kyusyukeigo.Helper;
+
+namespace SIGVerse.Common
+{
+	[InitializeOnLoad]
+	public class GameViewSizeInitializer
+	{
+		private static readonly GameViewSizeGroupType groupType = GameViewSizeGroupType.Standalone;
+		private static readonly GameViewSizeHelper.GameViewSizeType gameViewSizeType = GameViewSizeHelper.GameViewSizeType.AspectRatio;
+		private static readonly int width = 16;
+		private static readonly int height = 9;
+		private static readonly string text = "SIGVerse Default";
+
+		static GameViewSizeInitializer()
+		{
+			if(!GameViewSizeHelper.Contains(groupType, gameViewSizeType, width, height, text))
+			{
+				GameViewSizeHelper.AddCustomSize(groupType, gameViewSizeType, width, height, text);
+			}
+		}
+
+		[MenuItem("SIGVerse/Set Default GameView Size")]
+		protected static void SetDefaultGameViewSize()
+		{
+			GameViewSizeHelper.ChangeGameViewSize(groupType, gameViewSizeType, width, height, text);
+		}
+	}
+}
+
